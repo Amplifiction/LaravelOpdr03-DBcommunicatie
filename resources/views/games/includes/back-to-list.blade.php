d70ec7179da22db87cc61f6643fa3c8d39c2f431
Amplifiction <bart.stevens9@gmail.com>
2024-03-01 08:35:37 +0100

    comment bij ??

--- resources/views/games/includes/back-to-list.blade.php
+++ resources/views/games/includes/back-to-list.blade.php
@@ -1,7 +1,7 @@
 <div class="py-4 flex items-center gap-2">
     <a class="bg-blue-200 hover:bg-blue-300 text-blue-600 px-4 py-2 inline-block" href="{{ route('games.index') }}">&larr; Back to list</a>
 
-    @if($showLink ?? false)
+    @if($showLink ?? false) {{--if($showLink) geeft true or false. De ?? geeft aan wat te doen indien showLink niet is ingesteld. (Vermijden error.) Maw: default=false. --}}
         <a class="bg-blue-200 hover:bg-blue-300 text-blue-600 px-4 py-2 inline-block" href="{{ route('games.show', $game ) }}">&larr; Back to game</a>
     @endif
 </div>
